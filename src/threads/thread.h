97b87feb7dffef95952f5d72e43e93ebfe923cfc
h56983577 <h56983577@126.com>
2020-10-19 16:24:33 +0800

    refactor_将线程机制从忙等待修改为阻塞等待

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -89,6 +89,7 @@ struct thread
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
+    int64_t wait_ticks;
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
@@ -129,6 +130,9 @@ void thread_yield (void);
 /* Performs some operation on thread t, given auxiliary data AUX. */
 typedef void thread_action_func (struct thread *t, void *aux);
 void thread_foreach (thread_action_func *, void *);
+
+/* 若该线程处于blocked状态，则减少其等待时间1tick，需要在中断关闭时调用 */
+void thread_decrease_ticks (struct thread *t, void *aux);
 
 int thread_get_priority (void);
 void thread_set_priority (int);
